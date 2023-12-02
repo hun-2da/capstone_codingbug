b92aba18c8189b0cae8c75db8c0f308f34f92783
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-12-02 16:40:05 +0900

    커밋1

--- app/src/main/java/com/capstone/codingbug/pagerFragments/MyLocation_Fragment.java
+++ app/src/main/java/com/capstone/codingbug/pagerFragments/MyLocation_Fragment.java
@@ -7,15 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
+import com.skt.Tmap.TMapView;
+import com.skt.Tmap.TMapPolygon;
 
 import com.capstone.codingbug.R;
 
 public class MyLocation_Fragment extends Fragment {
-
+    private TMapView tMapView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my_location_, container, false);
+
+        LinearLayout linearLayoutTmap = view.findViewById(R.id.linearLayoutTmap);
+        tMapView = new TMapView(getActivity());
+
+        tMapView.setSKTMapApiKey("6QqIU9fnZUao65WJCM7ptafry6XfQovT1PoVoB4a");
+        linearLayoutTmap.addView(tMapView);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_location_, container, false);
+        return view;
     }
 }
